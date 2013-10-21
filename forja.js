2d3e3656304f3442dd041fc68bdcb4d1938bae07
David Torroija <you@example.com>
2013-10-21 17:14:56 -0500

    several changes and refactor, test finished!

--- forja.js
+++ forja.js
@@ -1,36 +1,51 @@
-var input = ['Bua'],
+var input = 'Mua-Bua-Gua'//'Bua-Bua-Gua-Mua-Gua-Bua'
+, // (Mua-Bua), bua-gua , mua-bua-gua
 forjaApp = (function(input,undefined){
 	
+	var	that = {
+		getSleepHoursString: getSleepHoursString,
+		attendBaby: attendBaby,
+		sleepMinutes: 480,
+		previousInput: '',
+		prevPrevInput: '',
+		calculateLostFollowers: calculateLostFollowers,
+		//initialize: initialize
+		parseInput: parseInput
+	},
+	actions ='';
 	function attendBaby(input){
-		var action;		
-		var cases = {
+		var action,		
+			cases = {
 			Bua: function() { 
-				action = 'feed';
+				action = 'Alimentar';
 
 				if (that.previousInput==='Mua'){
 					that.sleepMinutes = that.sleepMinutes - 45;	
+					console.log('Mua-Bua');
 				}
 				else {				
 					that.sleepMinutes = that.sleepMinutes - 60;
 				}
 			},
 			Mua: function(){
-				action = 'change';
+				action = 'Pañal';
 				that.sleepMinutes = that.sleepMinutes - 30;
 			},
 			Gua: function(){
-				action = 'pacifier';
+				action = 'Chupete';
 
 				if (that.previousInput==='Bua'&&that.prevPrevInput==='Mua'){
+					console.log('-Mua-Bua-Gua');
 					return;
 				}
 				if (that.previousInput==='Bua'){
+					console.log('Bua-Gua');
 					that.sleepMinutes = that.sleepMinutes - 5					
 				}else{
 					that.sleepMinutes = that.sleepMinutes - 10;						
 				}
 			},
-			_default: function() { console.log(input); }
+			_default: function() { console.log(input,'input'); }
 		};
 		
 		cases[ input ] ? cases[ input ]() : cases._default();
@@ -39,28 +54,52 @@ forjaApp = (function(input,undefined){
 		that.previousInput = input;
 		return action;				
 	}
-	function getSleepHours(){
-		return sleepMinutes / 60;	
+	function getSleepHoursString(){
+		return ((that.sleepMinutes - (that.sleepMinutes % 60)) / 60).toString() +' horas, '+ ((that.sleepMinutes % 60)) +' minutos' ;	
 	}
-	// function parseInput(){
-	// 	for (var i = 0; i < input.length; i++) {
-	// 		attendBaby(input[i]);
-	// 	};
-	// }
+	function calculateLostFollowers(){
+		
+		if(that.sleepMinutes>=240){ 
+			//hours >=4
+			return 0;	
+		}
+		else{
+			var minutes = that.sleepMinutes, lostFollowers = 0;
+				
+			if(that.sleepMinutes>=120&&that.sleepMinutes<240){
+				//hours >= 2 and hours < 4
+				while(minutes<240){
+					minutes += 30;
+					lostFollowers +=100;
+				}
+				
+			}
+			if(that.sleepMinutes>=0&&that.sleepMinutes<120){
+				//hours >= 0 and hours < 2
+				lostFollowers = 400;
+				while(minutes<120){
+					minutes += 30;
+					lostFollowers +=200;
+				}
+				
+			}
+			return lostFollowers;			
+		}
+	}
 
-	// parseInput(input);
-	// function initialize(spec){
-	// 	var spec
-	// }
-	that = {
-		getSleepHours: getSleepHours,
-		attendBaby: attendBaby,
-		sleepMinutes: 480,
-		previousInput: '',
-		prevPrevInput: ''
-		//initialize: initialize
-		//parseInput: parseInput
-	};
+	function parseInput(){
+		that.sleepMinutes = 480;
+		var input = window.input.split('-');
+		for (var i = 0; i < input.length; i++) {
+			actions += ' -' + attendBaby(input[i]);
+			//console.log(forjaApp.sleepMinutes);
+		};
+		console.log('calculateLostFollowers', that.calculateLostFollowers(), ',sleepMinutes:',that.sleepMinutes, ',getSleepHoursString', that.getSleepHoursString(),',actions:', actions,',that.sleepMinutes/60', that.sleepMinutes/60);
+	}
+
+	parseInput(window.input);
+	
+		
 	return that;
 
 })(input);
--- tests/testForja.js
+++ tests/testForja.js
@@ -1,87 +1,64 @@
 (function (window){
-	describe('Baby care App',function(){
+	describe('Baby care App - ',function(){
 		var input;
 		beforeEach(function(){
 			forjaApp.sleepMinutes = 0;			
 		});		
 
-		describe('Baby Care Part',function(){
+		describe('Baby Care Part -',function(){
 			describe('input === "Bua"',function(){			
-				it('should attendBaby return "feed"',function(){			
-					expect(forjaApp.attendBaby('Bua')).toBe('feed');
+				it('should attendBaby return "Alimentar"',function(){			
+					expect(forjaApp.attendBaby('Bua')).toBe('Alimentar');
 				});
 				it('should attendBaby substract 60 minutes of sleep ',function(){			
 					forjaApp.attendBaby('Bua');		
-					console.log(forjaApp.sleepMinutes);		
 					expect(forjaApp.sleepMinutes).toBe(-60);
 				});
 			});
 
 			describe('input === "Mua"',function(){			
-				it('should attendBaby return "change"',function(){			
-					expect(forjaApp.attendBaby('Mua')).toBe('change');
+				it('should attendBaby return "Pañal"',function(){			
+					expect(forjaApp.attendBaby('Mua')).toBe('Pañal');
 				});
 				it('should attendBaby substract 30 minutes of sleep ',function(){			
 					forjaApp.attendBaby('Mua');		
-					console.log(forjaApp.sleepMinutes);		
 					expect(forjaApp.sleepMinutes).toBe(-30);
 				});			
 			});		
 			
 			describe('input === "Gua"',function(){			
-				it('should attendBaby return "pacifier"',function(){			
-					expect(forjaApp.attendBaby('Gua')).toBe('pacifier');
+				it('should attendBaby return "Chupete"',function(){			
+					expect(forjaApp.attendBaby('Gua')).toBe('Chupete');
 				});
 				it('should attendBaby substract 10 minutes of sleep ',function(){			
 					forjaApp.attendBaby('Gua');		
-					console.log(forjaApp.sleepMinutes);		
 					expect(forjaApp.sleepMinutes).toBe(-10);
 				});			
 			});	
 
 			describe('We need a property to store the previous input state',function(){
 				it('should previousInput to be "Mua" when we call attendBaby with "Mua" Input',function(){			
-					expect(forjaApp.attendBaby('Mua')).toBe('change');
+					expect(forjaApp.attendBaby('Mua')).toBe('Pañal');
 					expect(forjaApp.previousInput).toBe('Mua');				
 				});
 			});
 
-			describe('input === "Mua" and then input === "Bua"',function(){						
+			describe('(Mua-Bua) - input === "Mua" and then input === "Bua"',function(){						
 				it('should attendBaby called twice with this two inputs substract 75 minutes of sleep ',function(){			
 					forjaApp.attendBaby('Mua');	
 					forjaApp.attendBaby('Bua');		
-					console.log(forjaApp.sleepMinutes);		
 					expect(forjaApp.sleepMinutes).toBe(-75);
 				});
 				
 			});	
 
-			describe('input === "Bua" and then input === "Gua"',function(){			
+			describe('(Bua-Gua) - input === "Bua" and then input === "Gua"',function(){			
 				it('should attendBaby called twice with this two inputs substract 65 minutes of sleep ',function(){			
 					forjaApp.attendBaby('Bua');	
 					forjaApp.attendBaby('Gua');		
-					console.log(forjaApp.sleepMinutes);		
 					expect(forjaApp.sleepMinutes).toBe(-65);
 				});
-			});	
-
-			describe('input === "Bua" and then input === "Gua"',function(){			
-				it('should attendBaby called twice with this two inputs substract 65 minutes of sleep ',function(){			
-					forjaApp.attendBaby('Bua');	
-					forjaApp.attendBaby('Gua');		
-					console.log(forjaApp.sleepMinutes);		
-					expect(forjaApp.sleepMinutes).toBe(-65);
-				});
-			});	
-
-			describe('input === "Bua" and then input === "Gua"',function(){			
-				it('should attendBaby called twice with this two inputs substract 65 minutes of sleep ',function(){			
-					forjaApp.attendBaby('Bua');	
-					forjaApp.attendBaby('Gua');		
-					console.log(forjaApp.sleepMinutes);		
-					expect(forjaApp.sleepMinutes).toBe(-65);
-				});
-			});	
+			});				
 
 			describe('We need a property to store the previous of the previous input state',function(){
 				it('should previousInput to be "Mua" and the prevPrevInput to be',function(){			
@@ -92,19 +69,62 @@
 				});
 			});
 
-			describe('input === "Mua" and then input === "Bua" and then input === "Gua"',function(){
+			describe('(Mua-Bua-Gua) - input === "Mua" and then input === "Bua" and then input === "Gua"',function(){
 				it('should attendBaby called with this three different inputs substract -75 minutes of sleep ',function(){			
 					forjaApp.attendBaby('Mua');	
 					forjaApp.attendBaby('Bua');	
 					forjaApp.attendBaby('Gua');	
-					console.log(forjaApp.sleepMinutes);		
 					expect(forjaApp.sleepMinutes).toBe(-75);
 				});
 			});	
 		});		
 
-		describe('Followers Part',function(){
+		describe('Followers Part -',function(){
+
+			// it('should start with 0 looseFollowers',function(){
+
+			// });
 			
+			describe('We have 4 or more sleep hours - ',function(){
+				it('should not loose any Followers!',function(){
+					forjaApp.sleepMinutes = 4 * 60;
+					expect(forjaApp.calculateLostFollowers()).toBe(0);
+					forjaApp.sleepMinutes = 8 * 60;
+					expect(forjaApp.calculateLostFollowers()).toBe(0);
+				});
+			});
+			describe('We have less than 4 sleep hours - ',function(){
+				describe('Slept 1 to 30 less minutes - ',function(){
+					it('should loose 100 followers',function(){
+						forjaApp.sleepMinutes = (4 * 60) - 30;
+						expect(forjaApp.calculateLostFollowers()).toBe(100);
+						forjaApp.sleepMinutes = (4 * 60) - 1;
+						expect(forjaApp.calculateLostFollowers()).toBe(100);
+					});
+				});	
+				describe('Slept 2 to 3.5 hours - ',function(){
+					it('should loose 100 followers every 30 minutes',function(){
+						forjaApp.sleepMinutes = (4 * 60) - 60;
+						expect(forjaApp.calculateLostFollowers()).toBe(200);
+						forjaApp.sleepMinutes = (4 * 60) - 90;						
+						expect(forjaApp.calculateLostFollowers()).toBe(300);
+						forjaApp.sleepMinutes = (4 * 60) - 120;						
+						expect(forjaApp.calculateLostFollowers()).toBe(400);
+					});
+				});
+				describe('Slept 0 to 2 hours - ',function(){
+					it('should loose 200 followers every 30 minutes plus 400 for the 2 hours first two hours',function(){
+						forjaApp.sleepMinutes = (2 * 60) - 30;
+						expect(forjaApp.calculateLostFollowers()).toBe(600);
+						forjaApp.sleepMinutes = (2 * 60) - 60;
+						expect(forjaApp.calculateLostFollowers()).toBe(800);
+						forjaApp.sleepMinutes = (2 * 60) - 90;						
+						expect(forjaApp.calculateLostFollowers()).toBe(1000);
+						forjaApp.sleepMinutes = (2 * 60) - 120;						
+						expect(forjaApp.calculateLostFollowers()).toBe(1200);
+					});
+				});					
+			});
 		});
 	});
 }
